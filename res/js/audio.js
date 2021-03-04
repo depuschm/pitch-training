23796b508528cb9c8cc4712ebd5791ed84a7c515
Dietmar Puschmann <depuschm@gmx.de>
2021-03-04 19:11:17 +0100

    Added sounds for all midi instruments

--- res/js/audio.js
+++ res/js/audio.js
@@ -1,26 +1,45 @@
-let generalMIDINumber = 0;
-let generalMIDIInstrument = 'acoustic_grand_piano';
+let instrumentSelect = document.getElementById("instrument");
+let instrument = instrumentSelect.value.split(':');
+
+let generalMIDINumber = instrument[0];
+let generalMIDIInstrument = instrument[1];
 
 let delay = 0; // play one note every quarter second
 let velocity = 127; // how hard the note hits
 let volume = 63; // how loud the note is
 
-// Midi instrument codes:
-// https://en.wikipedia.org/wiki/General_MIDI
 window.onload = function () {
+	instrumentSelect.addEventListener('change', function() {
+		instrument = instrumentSelect.value.split(':');
+		generalMIDINumber = instrument[0];
+		generalMIDIInstrument = instrument[1];
+		LoadMIDIPlugin();
+	});
+	LoadMIDIPlugin();
+};
+
+/**
+ * MIDI instrument codes:
+ * https://en.wikipedia.org/wiki/General_MIDI
+ *
+ * Alternative external soundfont URL:
+ * "http://www.song-data.com/3rd/MIDIjs/soundfont/"
+ */
+function LoadMIDIPlugin() {
 	MIDI.loadPlugin({
-		//soundfontUrl: "res/soundfont/",
-		soundfontUrl: "http://www.song-data.com/3rd/MIDIjs/soundfont/",
+		soundfontUrl: "res/soundfont/",
 		instrument: generalMIDIInstrument,
 		onsuccess: function() {
-			//MIDI.programChange(0, 25);
+			MIDI.programChange(0, generalMIDINumber);
 			MIDI.setVolume(0, volume);
 		}
 	});
-};
+}
 
-// Midi note table (Piano with 88 keys starts with index 21):
-// https://www.inspiredacoustics.com/en/MIDI_note_numbers_and_center_frequencies
+/**
+ * MIDI note table (Piano with 88 keys starts with index 21):
+ * https://www.inspiredacoustics.com/en/MIDI_note_numbers_and_center_frequencies
+ */
 function playAudio(parent, noteIndex, octave) {
 	let noteName = notesWhite[noteIndex];
 	if (parent == buttonsBlack) noteName = notesBlack[noteIndex];
